cb19fcb059bb55c156a06e7e621c9d13017bf945
JP Lynch <JPLynch35@gmail.com>
2018-06-05 09:33:28 -0600

    adds pantry methods and tests

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,23 @@
-class Pantry
+require './lib/recipe'
 
+class Pantry
+  attr_reader :stock,
+              :shopping_list
+
+  def initialize
+    @stock = Hash.new(0)
+    @shopping_list = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, quantity)
+    @stock[ingredient] = @stock[ingredient] + quantity
+  end
+
+  def add_to_shopping_list(recipe)
+    @shopping_list = recipe.ingredients.merge(@shopping_list)
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,71 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 
+  def test_stock_starts_empty
+    pantry = Pantry.new
+    expected = {}
+    assert_equal expected, pantry.stock
+  end
+
+  def test_stock_check_checks_stock_of_empty_pantry
+    pantry = Pantry.new
+    actual = pantry.stock_check('Cheese')
+    assert_equal 0, actual
+  end
+
+  def test_can_restock_ingredients
+    pantry = Pantry.new
+    actual = pantry.stock_check('Cheese')
+    assert_equal 0, actual
+    pantry.restock('Cheese', 10)
+    actual = pantry.stock_check('Cheese')
+    assert_equal 10, actual
+  end
+
+  def test_can_add_more_stock_to_exisiting_stock
+    pantry = Pantry.new
+    pantry.restock('Cheese', 10)
+    actual = pantry.stock_check('Cheese')
+    assert_equal 10, actual
+    pantry.restock('Cheese', 20)
+    actual = pantry.stock_check('Cheese')
+    assert_equal 30, actual
+  end
+
+  def test_can_add_a_recipe_to_shopping_list
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    expected = {}
+    assert_equal expected, pantry.shopping_list
+    r.add_ingredient("Flour", 20)
+    r.add_ingredient("Cheese", 20)
+    pantry.add_to_shopping_list(r)
+    expected =  {
+                'Cheese' => 20,
+                'Flour' => 20
+                }
+    assert_equal expected, pantry.shopping_list
+  end
+
+  def test_can_add_multiple_recipes_to_shopping_list
+    pantry = Pantry.new
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Flour", 20)
+    r1.add_ingredient("Cheese", 20)
+    pantry.add_to_shopping_list(r1)
+    r2 = Recipe.new("Spaghetti")
+    r2.add_ingredient("Spaghetti Noodles", 10)
+    r2.add_ingredient("Marinara Sauce", 10)
+    r2.add_ingredient("Cheese", 5)
+    pantry.add_to_shopping_list(r2)
+
+  end
 end
